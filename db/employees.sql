025964b4abfb93f7db0cdcf675b79ac7f5e1ee4f
Spazcool <douglas.wrightiii@gmail.com>
2020-06-15 21:52:20 -0400

    update schemas

--- db/employees.sql
+++ db/employees.sql
@@ -1,13 +1,13 @@
-USE cms_db;
+-- USE cms_db;
 
-CREATE TABLE employees(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES roles(role_id) NOT NULL,
-    FOREIGN KEY (manger_id) REFERENCES employees(manager_id),
-    PRIMARY KEY (id)
-)
+-- CREATE TABLE employees(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     FOREIGN KEY (role_id) REFERENCES roles(role_id) NOT NULL,
+--     FOREIGN KEY (manger_id) REFERENCES employees(manager_id),
+--     PRIMARY KEY (id)
+-- )
 
 -- INSERT INTO employees (first_name, last_name, role_id, manager_id)
 -- VALUES 
--- db/roles.sql
+++ db/roles.sql
@@ -1,16 +1,16 @@
-USE cms_db;
+-- USE cms_db;
 
-CREATE TABLE roles(
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES departments(department_id),
-    PRIMARY KEY (id)
-)
+-- CREATE TABLE roles(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     title VARCHAR(30) NOT NULL,
+--     salary DECIMAL NOT NULL,
+--     FOREIGN KEY (department_id) REFERENCES departments(department_id),
+--     PRIMARY KEY (id)
+-- )
 
 -- INSERT INTO roles (title, salary, department_id)
 -- VALUES 
---     ("Marketing Dude", 12.50, 1),
---     ("Development Dudette", 50.7, 2),
---     ("Customer Relations Squire", 8.1, 3),
+--     ("Manager", 12.50, 1),
+--     ("Intern", 50.7, 2),
+--     ("Engineer", 8.1, 3),
 --     ("Sales Maestro", 2.344, 4);
