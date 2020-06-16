78c8399aade3fc8e13c925e59582c24c0f4e1593
Spazcool <douglas.wrightiii@gmail.com>
2020-06-15 22:19:10 -0400

    mvc for queries done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const { Table } = require('console-table-printer');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
@@ -13,16 +14,23 @@ const connection = mysql.createConnection({
 connection.connect( (err) => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
-    // viewTable('departments');
-    addRow("employees", 
+    // addRow("employees", 
+    //     {
+    //         "first_name" : "p",
+    //         "last_name" : "diddy",
+    //         "role_id" : 2,
+    //         "manager_id" : 5
+    //     }
+    // );
+
+    updateRow("roles", 2, 
         {
-            "first_name" : "p",
-            "last_name" : "diddy",
-            "role_id" : 2,
-            "manager_id" : 5
+            "title" : "Intern",
+            "salary" : 15,
+            "department_id" : 2
         }
     );
-    // updateRow("departments", department_id, ["Marketing Team"]);
+    viewTable('roles');
     // // ---
     // updateManager(employee_id, new_manager_id);
     // viewTeam(manager_id); // and join the employees that share that id
@@ -35,15 +43,34 @@ connection.connect( (err) => {
 function viewTable(table) {
     connection.query("SELECT * FROM ??;",[table], (err, res) => {
         if (err) throw err;
+        // const p = new Table();
+        // p.addRow({ index: 1, text: 'red wine', value: 10.212 }, { color: 'red' });
+        // p.addRow({ index: 2, text: 'green gemuse', value: 20.0 }, { color: 'green' });
+        // p.addRow({ index: 3, text: 'gelb bananen', value: 100 }, { color: 'yellow' });
+        // p.printTable();
         console.log(res);
     });
 }
+
 function addRow(table, obj) {
     let keys = Object.keys(obj);
     let vals = keys.map((key) => obj[key]);
 
+    keys.forEach((key) => {})
     connection.query("INSERT INTO ?? (??) VALUES (?);", [table, keys, vals], (err, res) => {
         if (err) throw err;
         console.log(`Added | ${vals.join(' | ')} | successfully!`);
     });
+}
+
+function updateRow(table, row, obj) {
+    let keys = Object.keys(obj);
+    let vals = keys.map((key) => obj[key]);
+// todo might not work with latency, might need to be async
+    keys.forEach((key) => {
+        connection.query("UPDATE ?? SET ?? = ? WHERE id = ?;", [table, key, obj[key], row], (err, res) => {
+            if (err) throw err;
+        });
+    });
+    console.log(`Updated | ${vals.join(' | ')} | successfully!`);
 }
